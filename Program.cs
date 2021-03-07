08bf63231c7929072514282ba584ea07fd918b3f
RANIELE SILVA <comraniele@hotmail.com>
2021-03-07 19:03:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SomaSimples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int A, B, soma;
+
+			Console.Write("Digite o primeiro numero: ");
+			A = int.Parse(Console.ReadLine());
+			Console.Write("Digite o segundo numero: ");
+			B = int.Parse(Console.ReadLine());
+
+			soma = A + B;
+
+			Console.WriteLine("SOMA = " + soma);
+
+			Console.ReadLine();
+		}
+	}
+}
